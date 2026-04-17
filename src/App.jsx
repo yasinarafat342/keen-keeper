d1f321b12d80bba5e7b7db86947707896aab796b
yasinarafat342 <yasingazi343@gmail.com>
2026-04-17 23:28:40 +0600

    adding pics

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,27 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Timeline from "./pages/Timeline";
 import Stats from "./pages/Stats";
-import FriendDetail from "./pages/FriendDetail"; // বানান ঠিক আছে তো?
-
+import FriendDetail from "./pages/FriendDetail";
+import { InteractionProvider } from "./InteractionContext"; // ১. কন্টেক্সট ইম্পোর্ট করলাম
 
 function App() {
   return (
-    <div className="min-h-screen flex flex-col font-['Inter',sans-serif] bg-[#F9FAFB]">
-      <Navbar />
-      <main className="flex-grow">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/timeline" element={<Timeline />} />
-          <Route path="/stats" element={<Stats />} />
-          {/* Dynamic Route for Friend Details */}
-          <Route path="/friend/:id" element={<FriendDetail />} />
-        </Routes>
-      </main>
-      <Footer />
-    </div>
+    /* ২. পুরো অ্যাপকে InteractionProvider দিয়ে মুড়িয়ে দিলাম যাতে সব পেজ ডাটা পায় */
+    <InteractionProvider>
+      <div className="min-h-screen flex flex-col font-['Inter',sans-serif] bg-[#F9FAFB]">
+        <Navbar />
+        <main className="flex-grow">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/timeline" element={<Timeline />} />
+            <Route path="/stats" element={<Stats />} />
+            {/* Dynamic Route for Friend Details */}
+            <Route path="/friend/:id" element={<FriendDetail />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </InteractionProvider>
   );
 }
 
